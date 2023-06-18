0f973f05a8f9c746c65600c5175e0f9b91fb3866
cc81972 <coreychen.mp2@gmail.com>
2023-06-18 15:18:22 -0700

    added the foward and back prop functions along with activation functions

--- main.py
+++ main.py
@@ -15,16 +15,85 @@ from matplotlib import pyplot as plt
 #dW2 = 1/m dZ A1^T Derivative of loss function w respect to loss function
 #db2 = the error 
 
-data = pd.read_csv('mnist_test.csv')
-print(data.head())
+data = pd.read_csv('mnist_test.csv') 
 data = np.array(data)
 m,n = data.shape
-np.random.shuffle(data)
+np.random.shuffle(data) #Randomize the data
 
-data_dev = data[0:1000].T
+data_dev = data[0:1000].T #Transposing the data
 Y_dev = data_dev[0]
 X_dev = data_dev[1:n]
 
-data_train = data[1000:m].T
+data_train = data[1000:m].T #Data that will be used in training
 Y_train = data_train[0]
 X_train = data_train[1:n]
+
+
+def init_paramters(): #Main function to initialize all the parameters
+    #Including the weights and biases
+    W1 = np.random.rand(10,784) - 0.5 #First weight
+    b1 = np.random.rand(10,1) - 0.5 #First bias
+    W2 = np.random.rand(10,10) - 0.5 #Second weight
+    b2 = np.random.rand(10,1) - 0.5 #Second bias
+    return W1, b1, W2, b2
+
+def ReLU(Z): #This is our ReLU activation function
+    return np.maximum(0,Z)
+
+def softmax(Z): #This is our softmax activation function
+    return np.exp(Z) / np.sum(np.exp(Z))
+
+def foward_prop(W1,b1,W2,b2,X): #Foward propogation function
+    Z1 = W1.dot(X) + b1
+    A1 = ReLU(Z1)
+    Z2 = W2.dot(A1) + b2
+    A2 = softmax(Z2)
+    return Z1, A1, Z2, A2
+
+def one_hot(Y): #One hot encoding our Y matrix
+    one_hot_Y = np.zeros((Y.size, Y.max() + 1))
+    one_hot_Y[np.arange(Y.size), Y] = 1
+    one_hot_Y = one_hot_Y.T #Take the transpose of the matrix to make it column
+    return one_hot_Y
+
+def deriv_ReLU(Z): #Taking the derivative of our RelU function
+    return Z > 0 #If Z > 0, will return as 1 which is the slope of our ReLU function
+
+def back_prop(Z1, A1, Z2, A2, W2, Y): #Main back propogation function
+    m = Y.size
+    one_hot_Y = one_hot(Y)
+    dZ2 = A2 - one_hot_Y
+    dW2 = 1/m *dZ2.dot(A1.T)
+    dB2 = 1/m * np.sum
+    dZ1 = W2.T.dot(dZ2) * deriv_ReLU(Z1)
+    dW1 = 1/m * dZ2.dot(X.T)
+    db1 = 1/m * np.sum(dZ2, 2)
+    return dW1, db1, dW2, dB2
+
+def update_params(W1, b1, W2, b2, dW1, db1, dW2, db2, alpha):
+    W1 = W1 - alpha * dW1
+    b1 = b1 - alpha * db1
+    W2 = W2 - alpha * dW2
+    b2 = b2 - alpha * db2
+    return W1, b1, W2,  b2
+
+def get_predicitons(A2):
+    return np.argmax(A2,0)
+
+def get_accuracy(predictions, Y): 
+    print(predictions, Y)
+    return np.sum(predictions == Y) / Y.size
+
+def gradient_descent(X, Y, iter, alpha):
+    W1, b1, W2, b2 = init_paramters()
+    for i in range(iter):
+        Z1, A1, Z2, A2 = foward_prop(W1, b1, W2, b2, X)
+        dW1, db1, dW2, db2 = back_prop(Z1, A1, Z2, A2, W2, X, Y)
+        W1, b1, W2, b2 = update_params(W1, b1, W2, b2, dW1, db1, dW2, db2, alpha)
+        if (i % 10 == 0):
+            print("Iteration: ", i)
+            print("Accuracy: ", get_accuracy(get_predicitons(A2), Y))
+    return W1, b1, W2, b2
+
+W1, b1, W2, b2 = gradient_descent(X_train, Y_train, 500, 0.1)
+
